c170a89f313852e9e0990e0c00cdfdb071348586
Mohammed Elnawawy <shekoelnawawy@gmail.com>
2025-08-28 00:18:50 -0700

    update 46

--- main.py
+++ main.py
@@ -1,101 +1,28 @@
+import csv
 from sklearn import preprocessing
 from sklearn.linear_model import LogisticRegression
 import pandas as pd
 import numpy as np
 import joblib
 from sklearn.cluster import KMeans
-import os
-import warnings
-from tqdm import tqdm
 
+import warnings
+import os
 warnings.filterwarnings("ignore")
 
-# base_dir = '/Users/nawawy/Desktop/Research/Sepsis_data'
-base_dir = '/home/mnawawy/Sepsis'
-training_sets = ['training_setA', 'training_setB']
-os.makedirs(os.path.join(base_dir, 'results', 'cluster_outputs'), exist_ok=True)
-
-# get feature names
-f = open(os.path.join(base_dir,'physionet.org/files/challenge-2019/1.0.0/training/training_setA/p000001.psv'), 'r')
-header = f.readline().strip()
-features = np.array(header.split('|')[:-1])
-f.close()
-
-benign_data_df = pd.DataFrame()
-adversarial_predictions_df = pd.DataFrame()
-
-for training_set in training_sets:
-    benign_data_path = os.path.join(base_dir, 'results', 'attack_outputs', training_set, 'Data', 'Benign')
-    adversarial_predictions_path = os.path.join(base_dir,  'results', 'attack_outputs', training_set, 'Predictions', 'Adversarial')
-    for f in tqdm(os.listdir(benign_data_path)):
-        if os.path.isfile(os.path.join(benign_data_path, f)) and not f.lower().startswith('.') and f.lower().endswith('pkl'):
-            benign_data = joblib.load(os.path.join(benign_data_path, f))
-            per_patient_df = pd.DataFrame(benign_data)
-            per_patient_df.columns = features
-            per_patient_df.insert(loc=0, column='PatientID', value=f[:-4])
-            benign_data_df = pd.concat([benign_data_df, per_patient_df], axis=0, ignore_index=True)
-        else:
-            raise Exception('Benign data file does not exist!')
-
-        predictions_f = f[:-4] + '.psv'
-        if os.path.isfile(os.path.join(adversarial_predictions_path, predictions_f)) and not predictions_f.lower().startswith('.') and predictions_f.lower().endswith('psv'):
-            adversarial_predictions_file = open(os.path.join(adversarial_predictions_path, predictions_f), 'r')
-            header = adversarial_predictions_file.readline().strip()
-            per_patient_df = pd.DataFrame(np.loadtxt(adversarial_predictions_file, delimiter='|')[:, 1])
-            adversarial_predictions_df = pd.concat([adversarial_predictions_df, per_patient_df], axis=0, ignore_index=True)
-        else:
-            raise Exception('Adversarial prediction file does not exist!')
-
-# pre-processing
-PatientIDs = benign_data_df['PatientID']
-selected_features = ['HR', 'O2Sat', 'Temp', 'SBP', 'MAP', 'DBP', 'Resp', 'Age', 'Gender']
-benign_data_df = benign_data_df.ffill()
-benign_data_df = benign_data_df.fillna(0)
-benign_data = np.array(benign_data_df.loc[:, selected_features])
-benign_data = preprocessing.normalize(benign_data)
-adversarial_output = np.array(adversarial_predictions_df)
-
-# logistic regression for feature importance
-# define dataset
-X = benign_data
-y = adversarial_output
-# define the model
-model = LogisticRegression()
-# fit the model
-model.fit(X, y)
-# get importance
-importance = model.coef_[0]
-
-timeseries = importance * benign_data
-
-risk_profiles = []
-risk_scores = []
-old_PatientID = PatientIDs[0]
-for j in range(len(timeseries)):
-    if PatientIDs[j] == old_PatientID:
-        risk_scores.append(float(sum(timeseries[j,:])))
-        if j == len(timeseries)-1:
-            risk_profiles.append((old_PatientID, risk_scores))
-    else:
-        risk_profiles.append((old_PatientID, risk_scores))
-        old_PatientID = PatientIDs[j]
-        risk_scores = []
-        risk_scores.append(float(sum(timeseries[j,:])))
-
-
-joblib.dump(risk_profiles, os.path.join(base_dir, 'results', 'cluster_outputs', 'RiskProfiles.pkl'))
-
-unique_PatientIDs = [item[0] for item in risk_profiles]
-df = pd.DataFrame([item[1] for item in risk_profiles]).ffill(axis=1)
-model = KMeans(n_clusters=2, verbose = 1)
+base_dir = '/home/mnawawy'
+df = joblib.load(os.path.join(base_dir, 'MortalityData/Data/RiskProfiles.pkl'))
+model = KMeans(n_clusters=2)
 model.fit(df)
+
 cluster_centers = model.cluster_centers_
 print(cluster_centers)
 
 from sklearn.metrics.pairwise import euclidean_distances
+
+# Calculate distances between all pairs of cluster centers
 distances_matrix = euclidean_distances(cluster_centers)
 print("Distance matrix between cluster centers:\n", distances_matrix)
-
 
 # import os
 # from sklearn.svm import OneClassSVM
