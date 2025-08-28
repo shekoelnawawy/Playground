17448aa92975170e9797835063343531bd7f3178
Mohammed Elnawawy <shekoelnawawy@gmail.com>
2025-08-27 22:36:11 -0700

    update 44

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ output_path = os.path.join(base_dir, 'Defenses', 'ResultsOneClassSVM')
 
 AllPatientsData = joblib.load(os.path.join(base_dir, 'results', 'attack_outputs', 'adversarial_data.pkl'))
 
-clf = OneClassSVM(kernel='poly',degree=3,gamma='auto',cache_size=4096, verbose=True, max_iter = 5000)
+clf = OneClassSVM(kernel='poly',degree=5,gamma='auto',cache_size=4096, verbose=True, max_iter = 5000)
 
 
 AllPatientIDs = joblib.load(os.path.join(base_dir, 'results', 'cluster_outputs', 'benign_data_adv_outputs', 'AllPatientIDs.pkl'))
@@ -29,93 +29,93 @@ MostVulnerablePatientIDs = joblib.load(os.path.join(base_dir, 'results', 'cluste
 MoreVulnerablePatientIDs = MostVulnerablePatientIDs
 LessVulnerablePatientIDs = list(set(AllPatientIDs) - set(MostVulnerablePatientIDs))
 ######################################################################################################################################
-# Samples
-os.makedirs(os.path.join(output_path, 'SamplesTraining'), exist_ok=True)
-results = open(os.path.join(output_path, 'SamplesTraining', 'Results.csv'), 'w')
-results.write('Run,Accuracy,Precision,Recall,F1\n')
-
-Accuracy = []
-Precision = []
-Recall = []
-F1 = []
-for run in range(5):
-    print('SamplesTraining\tTrial: ' + str(run))
-
-    split = train_test_split(AllPatientIDs, train_size=len(LessVulnerablePatientIDs))
-    train_indices = split[0]
-    test_indices = split[1][:math.floor(0.2 * len(AllPatientIDs))]
-
-    train = AllPatientsData[AllPatientsData['PatientID'].isin(train_indices)].drop(columns=['PatientID']).to_numpy()
-    train_x = train[:, :-1]
-    train_y = train[:, -1].astype(int)
-
-    test = AllPatientsData[AllPatientsData['PatientID'].isin(test_indices)].drop(columns=['PatientID']).to_numpy()
-    test_x = test[:, :-1]
-    test_y = test[:, -1].astype(int)
-
-    clf.fit(train_x, train_y)
-
-    lst = clf.predict(test_x)
-    lst[lst == 1] = 0
-    lst[lst == -1] = 1
-
-    Accuracy.insert(len(Accuracy), accuracy_score(test_y, lst) * 100)
-    Precision.insert(len(Precision), precision_score(test_y, lst))
-    Recall.insert(len(Recall), recall_score(test_y, lst))
-    F1.insert(len(F1), f1_score(test_y, lst))
-
-    results.write(
-        str(run) + ',' + str(accuracy_score(test_y, lst) * 100) + ',' + str(precision_score(test_y, lst)) + ',' + str(
-            recall_score(test_y, lst)) + ',' + str(f1_score(test_y, lst)) + '\n')
-
-results.write(
-    'Average,' + str(np.mean(Accuracy)) + ',' + str(np.mean(Precision)) + ',' + str(np.mean(Recall)) + ',' + str(
-        np.mean(F1)) + '\n')
-results.close()
-######################################################################################################################################
-# All patients
-os.makedirs(os.path.join(output_path, 'All'), exist_ok=True)
-results = open(os.path.join(output_path, 'All', 'Results.csv'), 'w')
-results.write('Cross-Validation,Accuracy,Precision,Recall,F1\n')
-
-cv = 0
-kf = KFold(n_splits=5)
-Accuracy = []
-Precision = []
-Recall = []
-F1 = []
-for train_indices, test_indices in kf.split(AllPatientIDs):
-    print('All\tCV: ' + str(cv))
-
-    train = AllPatientsData[AllPatientsData['PatientID'].isin([AllPatientIDs[i] for i in train_indices])].drop(columns=['PatientID']).to_numpy()
-    train_x = train[:, :-1]
-    train_y = train[:, -1].astype(int)
-
-    test = AllPatientsData[AllPatientsData['PatientID'].isin([AllPatientIDs[i] for i in test_indices])].drop(columns=['PatientID']).to_numpy()
-    test_x = test[:, :-1]
-    test_y = test[:, -1].astype(int)
-
-    clf.fit(train_x, train_y)
-
-    lst = clf.predict(test_x)
-    lst[lst == 1] = 0
-    lst[lst == -1] = 1
-
-    Accuracy.insert(len(Accuracy), accuracy_score(test_y, lst) * 100)
-    Precision.insert(len(Precision), precision_score(test_y, lst))
-    Recall.insert(len(Recall), recall_score(test_y, lst))
-    F1.insert(len(F1), f1_score(test_y, lst))
-
-    results.write(
-        str(cv) + ',' + str(accuracy_score(test_y, lst) * 100) + ',' + str(precision_score(test_y, lst)) + ',' + str(
-            recall_score(test_y, lst)) + ',' + str(f1_score(test_y, lst)) + '\n')
-
-    cv += 1
-
-results.write(
-    'Average,' + str(np.mean(Accuracy)) + ',' + str(np.mean(Precision)) + ',' + str(np.mean(Recall)) + ',' + str(
-        np.mean(F1)) + '\n')
-results.close()
+# # Samples
+# os.makedirs(os.path.join(output_path, 'SamplesTraining'), exist_ok=True)
+# results = open(os.path.join(output_path, 'SamplesTraining', 'Results.csv'), 'w')
+# results.write('Run,Accuracy,Precision,Recall,F1\n')
+#
+# Accuracy = []
+# Precision = []
+# Recall = []
+# F1 = []
+# for run in range(5):
+#     print('SamplesTraining\tTrial: ' + str(run))
+#
+#     split = train_test_split(AllPatientIDs, train_size=len(LessVulnerablePatientIDs))
+#     train_indices = split[0]
+#     test_indices = split[1][:math.floor(0.2 * len(AllPatientIDs))]
+#
+#     train = AllPatientsData[AllPatientsData['PatientID'].isin(train_indices)].drop(columns=['PatientID']).to_numpy()
+#     train_x = train[:, :-1]
+#     train_y = train[:, -1].astype(int)
+#
+#     test = AllPatientsData[AllPatientsData['PatientID'].isin(test_indices)].drop(columns=['PatientID']).to_numpy()
+#     test_x = test[:, :-1]
+#     test_y = test[:, -1].astype(int)
+#
+#     clf.fit(train_x, train_y)
+#
+#     lst = clf.predict(test_x)
+#     lst[lst == 1] = 0
+#     lst[lst == -1] = 1
+#
+#     Accuracy.insert(len(Accuracy), accuracy_score(test_y, lst) * 100)
+#     Precision.insert(len(Precision), precision_score(test_y, lst))
+#     Recall.insert(len(Recall), recall_score(test_y, lst))
+#     F1.insert(len(F1), f1_score(test_y, lst))
+#
+#     results.write(
+#         str(run) + ',' + str(accuracy_score(test_y, lst) * 100) + ',' + str(precision_score(test_y, lst)) + ',' + str(
+#             recall_score(test_y, lst)) + ',' + str(f1_score(test_y, lst)) + '\n')
+#
+# results.write(
+#     'Average,' + str(np.mean(Accuracy)) + ',' + str(np.mean(Precision)) + ',' + str(np.mean(Recall)) + ',' + str(
+#         np.mean(F1)) + '\n')
+# results.close()
+# ######################################################################################################################################
+# # All patients
+# os.makedirs(os.path.join(output_path, 'All'), exist_ok=True)
+# results = open(os.path.join(output_path, 'All', 'Results.csv'), 'w')
+# results.write('Cross-Validation,Accuracy,Precision,Recall,F1\n')
+#
+# cv = 0
+# kf = KFold(n_splits=5)
+# Accuracy = []
+# Precision = []
+# Recall = []
+# F1 = []
+# for train_indices, test_indices in kf.split(AllPatientIDs):
+#     print('All\tCV: ' + str(cv))
+#
+#     train = AllPatientsData[AllPatientsData['PatientID'].isin([AllPatientIDs[i] for i in train_indices])].drop(columns=['PatientID']).to_numpy()
+#     train_x = train[:, :-1]
+#     train_y = train[:, -1].astype(int)
+#
+#     test = AllPatientsData[AllPatientsData['PatientID'].isin([AllPatientIDs[i] for i in test_indices])].drop(columns=['PatientID']).to_numpy()
+#     test_x = test[:, :-1]
+#     test_y = test[:, -1].astype(int)
+#
+#     clf.fit(train_x, train_y)
+#
+#     lst = clf.predict(test_x)
+#     lst[lst == 1] = 0
+#     lst[lst == -1] = 1
+#
+#     Accuracy.insert(len(Accuracy), accuracy_score(test_y, lst) * 100)
+#     Precision.insert(len(Precision), precision_score(test_y, lst))
+#     Recall.insert(len(Recall), recall_score(test_y, lst))
+#     F1.insert(len(F1), f1_score(test_y, lst))
+#
+#     results.write(
+#         str(cv) + ',' + str(accuracy_score(test_y, lst) * 100) + ',' + str(precision_score(test_y, lst)) + ',' + str(
+#             recall_score(test_y, lst)) + ',' + str(f1_score(test_y, lst)) + '\n')
+#
+#     cv += 1
+#
+# results.write(
+#     'Average,' + str(np.mean(Accuracy)) + ',' + str(np.mean(Precision)) + ',' + str(np.mean(Recall)) + ',' + str(
+#         np.mean(F1)) + '\n')
+# results.close()
 ######################################################################################################################################
 # Most
 os.makedirs(os.path.join(output_path, 'Most'), exist_ok=True)
