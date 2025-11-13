55c2c03ad6b0194e789a8c457443e072b877d488
Mohammed Elnawawy <shekoelnawawy@gmail.com>
2025-11-12 21:52:07 -0800

    update 115

--- main.py
+++ main.py
@@ -1,25 +1,31 @@
 import joblib
 import math
 import os
+import shutil
 import numpy as np
 from pathlib import Path
 
 base_dir = Path("/home/mnawawy/Downloads/OhioT1DM/processed_data/")
+os.makedirs(os.path.join(base_dir, "drift", "patient", "2018data"), exist_ok=True)
+os.makedirs(os.path.join(base_dir, "drift", "patient", "2020data"), exist_ok=True)
 
 for file_path in base_dir.rglob("*data/*.pkl"):
-    # for file_path in src_dir.rglob("*.pkl"):
-        if file_path.name.endswith(".test.pkl"):
-            print(f"\n🔍 Checking: {file_path}")
-            # try:
-            #     obj = joblib.load(file_path)
-            #     try:
-            #         length = len(obj)
-            #         print(f"✅ Length: {length}")
-            #     except TypeError:
-            #         print("⚠️ Object has no measurable length (no __len__).")
-            #
-            #     obj['glucose'][math.floor(len(obj) * 0.7):] += np.random.randint(low=20, high=30, size=len(obj['glucose'][math.floor(len(obj) * 0.7):]))
-            #
-            # except Exception as e:
-            #     print(f"❌ Failed to load: {e}")
-        # else:
+    if file_path.name.endswith(".test.pkl"):
+        print(f"\n🔍 Checking: {file_path}")
+        # try:
+        #     obj = joblib.load(file_path)
+        #     try:
+        #         length = len(obj)
+        #         print(f"✅ Length: {length}")
+        #     except TypeError:
+        #         print("⚠️ Object has no measurable length (no __len__).")
+        #
+        #     obj['glucose'][math.floor(len(obj) * 0.7):] += np.random.randint(low=20, high=30, size=len(obj['glucose'][math.floor(len(obj) * 0.7):]))
+        #
+        # except Exception as e:
+        #     print(f"❌ Failed to load: {e}")
+    else:
+        if "2018" in file_path:
+            shutil.copyfile(file_path, os.path.join(base_dir, "drift", "patient", "2018data"))
+        elif "2020" in file_path:
+            shutil.copyfile(file_path, os.path.join(base_dir, "drift", "patient", "2020data"))
