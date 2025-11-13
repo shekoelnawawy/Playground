a836ea94709f4b7685c87b19f3046e70ac1df2f3
Mohammed Elnawawy <shekoelnawawy@gmail.com>
2025-11-12 22:01:06 -0800

    update 119

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ base_dir = Path("/home/mnawawy/Downloads/OhioT1DM/processed_data/")
 os.makedirs(os.path.join(base_dir, "drift", "patient", "2018data"), exist_ok=True)
 os.makedirs(os.path.join(base_dir, "drift", "patient", "2020data"), exist_ok=True)
 
-for file_path in base_dir.rglob("*data/*.pkl"):
+for file_path in base_dir.rglob("/*data/*.pkl"):
     if file_path.name.endswith(".test.pkl"):
         print(f"\n🔍 Checking: {file_path}")
         # try:
