8f12dea4f50a84966f3bc129ca1997252e5bcb81
Mohammed Elnawawy <shekoelnawawy@gmail.com>
2025-11-12 21:39:37 -0800

    update 112

--- main.py
+++ main.py
@@ -8,17 +8,18 @@ base_dir = Path("/home/mnawawy/Downloads/OhioT1DM/processed_data/")
 years = ["2020data", "2018data"]
 
 for src_dir in base_dir.rglob("*data"):
-    for file_path in src_dir.rglob("*.test.pkl"):
-        print(f"\n🔍 Checking: {file_path}")
-        # try:
-        #     obj = joblib.load(file_path)
-        #     try:
-        #         length = len(obj)
-        #         print(f"✅ Length: {length}")
-        #     except TypeError:
-        #         print("⚠️ Object has no measurable length (no __len__).")
-        #
-        #     obj['glucose'][math.floor(len(obj) * 0.7):] += np.random.randint(low=20, high=30, size=len(obj['glucose'][math.floor(len(obj) * 0.7):]))
-        #
-        # except Exception as e:
-        #     print(f"❌ Failed to load: {e}")
+    for file_path in src_dir.rglob("*.pkl"):
+        if file_path.name.endswith(".test.pkl"):
+            print(f"\n🔍 Checking: {file_path}")
+            # try:
+            #     obj = joblib.load(file_path)
+            #     try:
+            #         length = len(obj)
+            #         print(f"✅ Length: {length}")
+            #     except TypeError:
+            #         print("⚠️ Object has no measurable length (no __len__).")
+            #
+            #     obj['glucose'][math.floor(len(obj) * 0.7):] += np.random.randint(low=20, high=30, size=len(obj['glucose'][math.floor(len(obj) * 0.7):]))
+            #
+            # except Exception as e:
+            #     print(f"❌ Failed to load: {e}")
