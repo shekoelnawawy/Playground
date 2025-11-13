7bbad01be628a2723df460c031918829ac71a829
Mohammed Elnawawy <shekoelnawawy@gmail.com>
2025-11-12 22:15:34 -0800

    update 125

--- main.py
+++ main.py
@@ -11,18 +11,15 @@ os.makedirs(os.path.join(base_dir, "drift", "patient", "2020data"), exist_ok=Tru
 
 for file_path in base_dir.rglob("*data/*.pkl"):
     if file_path.name.endswith(".test.pkl"):
-        print(f"\n🔍 Checking: {file_path}")
         try:
             obj = joblib.load(file_path)
             obj.loc[math.floor(len(obj) * 0.7):, "glucose"] += np.random.randint(low=20, high=30, size=len(obj['glucose'][math.floor(len(obj) * 0.7):]))
             if "2018" in str(file_path):
                 dst_path = Path(os.path.join(base_dir, "drift", "patient", "2018data", file_path.name))
-                print(obj)
-                exit(1)
-                joblib.dump(obj)
+                joblib.dump(obj, dst_path)
             elif "2020" in str(file_path):
                 dst_path = Path(os.path.join(base_dir, "drift", "patient", "2020data", file_path.name))
-
+                joblib.dump(obj, dst_path)
         except Exception as e:
             print(f"Failed to load: {e}")
     else:
