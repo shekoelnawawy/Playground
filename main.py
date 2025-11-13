732a2435954040d4c3888e40fd78b5d1b2e6a41e
Mohammed Elnawawy <shekoelnawawy@gmail.com>
2025-11-12 22:11:42 -0800

    update 122

--- main.py
+++ main.py
@@ -12,18 +12,19 @@ os.makedirs(os.path.join(base_dir, "drift", "patient", "2020data"), exist_ok=Tru
 for file_path in base_dir.rglob("*data/*.pkl"):
     if file_path.name.endswith(".test.pkl"):
         print(f"\n🔍 Checking: {file_path}")
-        # try:
-        #     obj = joblib.load(file_path)
-        #     try:
-        #         length = len(obj)
-        #         print(f"✅ Length: {length}")
-        #     except TypeError:
-        #         print("⚠️ Object has no measurable length (no __len__).")
-        #
-        #     obj['glucose'][math.floor(len(obj) * 0.7):] += np.random.randint(low=20, high=30, size=len(obj['glucose'][math.floor(len(obj) * 0.7):]))
-        #
-        # except Exception as e:
-        #     print(f"❌ Failed to load: {e}")
+        try:
+            obj = joblib.load(file_path)
+            obj['glucose'][math.floor(len(obj) * 0.7):] += np.random.randint(low=20, high=30, size=len(obj['glucose'][math.floor(len(obj) * 0.7):]))
+            if "2018" in str(file_path):
+                dst_path = Path(os.path.join(base_dir, "drift", "patient", "2018data", file_path.name))
+                print(obj)
+                exit(1)
+                joblib.dump(obj)
+            elif "2020" in str(file_path):
+                dst_path = Path(os.path.join(base_dir, "drift", "patient", "2020data", file_path.name))
+
+        except Exception as e:
+            print(f"Failed to load: {e}")
     else:
         if "2018" in str(file_path):
             dst_path = Path(os.path.join(base_dir, "drift", "patient", "2018data", file_path.name))
