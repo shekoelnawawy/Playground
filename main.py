61fbc5a1b9fffdb5ab995c5164ff7ca0cb017dce
Mohammed Elnawawy <shekoelnawawy@gmail.com>
2025-11-12 22:21:10 -0800

    update 126

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ import shutil
 import numpy as np
 from pathlib import Path
 
+percent_drift = 30
+indexer = (100-percent_drift)/100
+
 base_dir = Path("/home/mnawawy/Downloads/OhioT1DM/processed_data/")
 os.makedirs(os.path.join(base_dir, "drift", "patient", "2018data"), exist_ok=True)
 os.makedirs(os.path.join(base_dir, "drift", "patient", "2020data"), exist_ok=True)
@@ -13,7 +16,7 @@ for file_path in base_dir.rglob("*data/*.pkl"):
     if file_path.name.endswith(".test.pkl"):
         try:
             obj = joblib.load(file_path)
-            obj.loc[math.floor(len(obj) * 0.7):, "glucose"] += np.random.randint(low=20, high=30, size=len(obj['glucose'][math.floor(len(obj) * 0.7):]))
+            obj.loc[math.floor(len(obj) * indexer):, "glucose"] += np.random.randint(low=20, high=30, size=len(obj['glucose'][math.floor(len(obj) * indexer):]))
             if "2018" in str(file_path):
                 dst_path = Path(os.path.join(base_dir, "drift", "patient", "2018data", file_path.name))
                 joblib.dump(obj, dst_path)
