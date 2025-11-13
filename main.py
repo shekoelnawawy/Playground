1d6f1ef8149a585d9fe37b09c7c02f1136140608
Mohammed Elnawawy <shekoelnawawy@gmail.com>
2025-11-12 21:36:00 -0800

    update 109

--- main.py
+++ main.py
@@ -1,71 +1,29 @@
-import os
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.svm import OneClassSVM
 import joblib
-import time
+import math
+import os
 import numpy as np
+from pathlib import Path
 
-base_dir = '/home/mnawawy/Sepsis'
-# base_dir = '/Users/nawawy/Desktop/Research/Sepsis_data'
+base_dir = Path("/home/mnawawy/Downloads/OhioT1DM/processed_data/")
+years = ["2020data", "2018data"]
 
-AllPatientsData = joblib.load(os.path.join(base_dir, 'results', 'attack_outputs', 'adversarial_data.pkl'))
+for src_dir in base_dir.rglob("*data"):
+    print("hi")
+exit(1)
+# src_dir = Path(os.path.join(base_dir,))
+# dst_dir =
 
-neigh = KNeighborsClassifier(n_neighbors=3)
-clf = OneClassSVM(gamma='scale', kernel='linear', verbose=True)
+for file_path in src_dir.rglob("*.test.pkl"):
+    print(f"\n🔍 Checking: {file_path}")
+    try:
+        obj = joblib.load(file_path)
+        try:
+            length = len(obj)
+            print(f"✅ Length: {length}")
+        except TypeError:
+            print("⚠️ Object has no measurable length (no __len__).")
 
-AllPatientIDs = joblib.load(os.path.join(base_dir, 'results', 'cluster_outputs', 'benign_data_adv_outputs', 'AllPatientIDs.pkl'))
-MostVulnerablePatientIDs = joblib.load(os.path.join(base_dir, 'results', 'cluster_outputs', 'benign_data_adv_outputs', 'threshold_10', 'MostVulnerablePatientIDs.pkl'))
-MoreVulnerablePatientIDs = joblib.load(os.path.join(base_dir, 'results', 'cluster_outputs', 'benign_data_adv_outputs', 'threshold_5', 'MoreVulnerablePatientIDs.pkl'))
-LessVulnerablePatientIDs = joblib.load(os.path.join(base_dir, 'results', 'cluster_outputs', 'benign_data_adv_outputs', 'threshold_5', 'LessVulnerablePatientIDs.pkl'))
+        obj['glucose'][math.floor(len(obj) * 0.7):] += np.random.randint(low=20, high=30, size=len(obj['glucose'][math.floor(len(obj) * 0.7):]))
 
-
-
-print('kNN')
-######################################################################################################################################
-# Least
-train = AllPatientsData[AllPatientsData['PatientID'].isin(LessVulnerablePatientIDs)].drop(columns=['PatientID']).to_numpy()
-train_x = train[:, :-1]
-train_y = train[:, -1].astype(int)
-start_train_time = time.perf_counter()
-neigh.fit(train_x, train_y)
-end_train_time = time.perf_counter()
-elapsed_train_time = end_train_time - start_train_time
-print(f"Total Training Time: {elapsed_train_time:.6f} seconds")
-
-test = AllPatientsData[AllPatientsData['PatientID'].isin(AllPatientIDs)].drop(columns=['PatientID']).to_numpy()
-test_x = test[:, :-1]
-test_y = test[:, -1].astype(int)
-
-start_test_time = time.perf_counter()
-lst = neigh.predict(test_x)
-end_test_time = time.perf_counter()
-elapsed_test_time = end_test_time - start_test_time
-print(f"Inference Time per instance: {elapsed_test_time/len(test):.6f} seconds")
-######################################################################################################################################
-
-print('------------------------------------------------------')
-print('One-Class SVM')
-######################################################################################################################################
-# Least
-train = AllPatientsData[AllPatientsData['PatientID'].isin(LessVulnerablePatientIDs)].drop(columns=['PatientID']).to_numpy()
-train_x = train[:, :-1]
-train_y = train[:, -1].astype(int)
-start_train_time = time.perf_counter()
-clf.fit(train_x, train_y)
-end_train_time = time.perf_counter()
-elapsed_train_time = end_train_time - start_train_time
-print(f"Total Training Time: {elapsed_train_time:.6f} seconds")
-
-test = AllPatientsData[AllPatientsData['PatientID'].isin(AllPatientIDs)].drop(columns=['PatientID']).to_numpy()
-test_x = test[:, :-1]
-test_y = test[:, -1].astype(int)
-
-start_test_time = time.perf_counter()
-lst = clf.predict(test_x)
-end_test_time = time.perf_counter()
-elapsed_test_time = end_test_time - start_test_time
-print(f"Inference Time per instance: {elapsed_test_time/len(test):.6f} seconds")
-lst[lst == 1] = 0
-lst[lst == -1] = 1
-
-######################################################################################################################################
+    except Exception as e:
+        print(f"❌ Failed to load: {e}")
