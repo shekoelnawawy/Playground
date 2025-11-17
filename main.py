03b898bea5c5da2e6ed1f8e680fb334bdd5c25b8
Mohammed Elnawawy <shekoelnawawy@gmail.com>
2025-11-16 16:01:32 -0800

    update 158

--- main.py
+++ main.py
@@ -1,36 +1,26 @@
-import math
-import os
-import shutil
-import numpy as np
-import pandas as pd
-from pathlib import Path
+import sys
 
-percent_drift = 30
-indexer = (100-percent_drift)/100
+outstr = ''
 
-base_dir = Path("/home/mnawawy/Downloads/Sepsis/files/challenge-2019/1.0.0/training")
-# out_dir = Path("/home/mnawawy/Downloads/Sepsis/processed_data/drift/patient")
-out_dir = Path("/home/mnawawy/SepsisDrift")
 
-os.makedirs(out_dir, exist_ok=True)
+def main():
+    print(outstr)
 
-shutil.copyfile(Path(base_dir / 'index.html'), Path(out_dir) / 'index.html')
+if __name__ == '__main__':
+    # Nawawy's start
+    # Parse arguments.
+    if len(sys.argv) != 4:
+        raise Exception('Include the input and output directories and train/test (0/1) flag as arguments, e.g., python run.py data output 1')
 
-for item_path in base_dir.rglob("*"):
-    if item_path.is_dir():  # Check if the item is a directory
-        directory_name = item_path.name
-        os.makedirs(os.path.join(out_dir, directory_name), exist_ok=True)
+    input_dir = sys.argv[1]
+    output_dir = sys.argv[2]
+    if sys.argv[3] == '1':
+        train = True
+    elif sys.argv[3] == '0':
+        train = False
+    else:
+        raise ValueError("Train flag must be 0 or 1")
 
-        for file_path in item_path.rglob("*"):
-            dst_path = Path(os.path.join(out_dir, directory_name, file_path.name))
-            if dst_path.name.endswith("*.psv"):
-                try:
-                    df = pd.read_csv(file_path, sep='|')
-                    df.loc[math.floor(len(df) * indexer):, "HR"] += np.random.randint(low=20, high=30, size=len(df["HR"][math.floor(len(df) * indexer):]))
-                    df.to_csv(dst_path, index=False, sep='|')
 
-                except Exception as e:
-                    print(f"Failed to load: {e}")
-            else:
-                if file_path.resolve() != dst_path.resolve():
-                    shutil.copyfile(file_path, dst_path)
+    # Nawawy's end
+    main()
